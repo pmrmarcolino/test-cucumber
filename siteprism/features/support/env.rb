6748844911404c371ff89ed23323e9e1c0f365b4
pmrmarcolino <pmrmarcolino@hotmail.com>
2022-03-29 14:49:37 -0300

    Utilizando headless

--- siteprism/features/support/env.rb
+++ siteprism/features/support/env.rb
@@ -11,9 +11,14 @@ ENVIRONMENT = ENV['ENVIRONMENT']
 
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environments/#{ENVIRONMENT}.yml")
 
+caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+    "goog:chromeOptions" => {"args" => ["--headless", "disable-gpu"]})
+
 Capybara.register_driver :selenium do |app|
     if BROWSER.eql? ('chrome')
         Capybara::Selenium::Driver.new(app, :browser => :chrome)
+    elsif BROWSER.eql? ('chrome_headless')
+        Capybara::Selenium::Driver.new(app, :browser => :chrome, desired_capabilities: caps)
     elsif BROWSER.eql? ('firefox')
         Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette =>true)
     elsif BROWSER.eql? ('safari')    
