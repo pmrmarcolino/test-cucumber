22bc18dba2892ca96b942187d7361f4f138dc3ad
pmrmarcolino <pmrmarcolino@hotmail.com>
2022-03-29 09:27:09 -0300

    Executando com varios navegadores

--- siteprism/features/support/env.rb
+++ siteprism/features/support/env.rb
@@ -5,11 +5,24 @@ require 'rspec'
 require_relative 'page_helper.rb'
 
 World(PageObjects)
+
+BROWSER = ENV['BROWSER']
 ENVIRONMENT = ENV['ENVIRONMENT']
+
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environments/#{ENVIRONMENT}.yml")
 
+Capybara.register_driver :selenium do |app|
+    if BROWSER.eql? ('chrome')
+        Capybara::Selenium::Driver.new(app, :browser => :chrome)
+    elsif BROWSER.eql? ('firefox')
+        Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette =>true)
+    elsif BROWSER.eql? ('safari')    
+        Capybara::Selenium::Driver.new(app, :browser => :safari)
+    end
+end
+
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+    config.default_driver = :selenium
     config.app_host = CONFIG['url']
-    config.default_max_wait_time = 10
+    config.default_max_wait_time = 15
 end
