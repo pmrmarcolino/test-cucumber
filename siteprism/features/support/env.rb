506436748a6d18a2e031ec7c43e8bec0231ae767
pmrmarcolino <pmrmarcolino@hotmail.com>
2022-03-29 08:54:10 -0300

    main

--- siteprism/features/support/env.rb
+++ siteprism/features/support/env.rb
@@ -5,8 +5,11 @@ require 'rspec'
 require_relative 'page_helper.rb'
 
 World(PageObjects)
+ENVIRONMENT = ENV['ENVIRONMENT']
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environments/#{ENVIRONMENT}.yml")
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
-    config.app_host = 'https://automacaocombatista.herokuapp.com'
+    config.app_host = CONFIG['url']
+    config.default_max_wait_time = 10
 end
